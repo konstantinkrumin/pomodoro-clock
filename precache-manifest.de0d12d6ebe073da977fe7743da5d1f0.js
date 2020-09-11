717f9743f55a9f4bbcfb80cc392df2590ee481d3
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-12 02:04:32 +0300

    Updates

---
+++ precache-manifest.de0d12d6ebe073da977fe7743da5d1f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053af0737417719bff65dde01401f37e",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "26aabba67c8c938f4a04",
+    "url": "/pomodoro-clock/static/css/main.e77d3cb7.chunk.css"
+  },
+  {
+    "revision": "b57451d44fe7d3db804a",
+    "url": "/pomodoro-clock/static/js/2.89afb521.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.89afb521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26aabba67c8c938f4a04",
+    "url": "/pomodoro-clock/static/js/main.bb541b77.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
